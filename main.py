93501a0ea2cd0c6c9c661824f733cba16f978a66
sizmosloth <samaybagga8@gmail.com>
2026-07-30 16:18:04 +0530

    Made some improvements and exception handling

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 from datetime import datetime
 
-def say_hello():
-    print("Greetingsss Master!!!")
+def say_hello(arg):
+    if arg == "":
+        print("Greetingsss Master!!!")
 
-def tell_time():
+def tell_time(arg):
     current_time = datetime.now().strftime("%I:%M %p").lstrip("0")
     print(f"Its {current_time}")
 
-def exit_assistant():
+def search(arg):
+        print(f"Searching for {arg} ...")
+
+def exit_assistant(arg):
     print("Sure Master, Byeee!")
 
-def unknown():
-    print("Sorry, I don't understand that command yet.")
+def unknown(arg):
+    print(f"Sorry, I don't understand ' {arg} '.")
 
 def main ():
 
     commands = {
         "hello" : say_hello,
         "time" : tell_time,
+        "search" : search,
         "exit" : exit_assistant,
     }
 
     while True:
         user_input = input("\nYou : ").strip().lower()    
+        part = user_input.split(maxsplit=1)
+        cmd = part[0]
+        arg = part[1] if len(part) > 1 else ""
 
-        if user_input in commands:
-            commands[user_input]()
-
-            if user_input == "exit":
-                break
+        if cmd in commands:
+            try:
+                commands[cmd](arg)
+            except Exception as e:
+                print(f"Something unsusal happened {e}")
 
         else:
-            unknown()
+            unknown(user_input)
+        if cmd == "exit":
+            break
 
 main ()
